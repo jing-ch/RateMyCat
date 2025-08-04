6a70f0eaaa9852a670c50efaa8038b20a2e4244b
jing <chenjinghanlaura@gmail.com>
2025-08-04 20:47:19 +0200

    refactored cats controller

---
+++ RateMyCat/Controllers/CatsController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RateMyCat.Data;
+using RateMyCat.Models;
+
+namespace RateMyCat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatsController : ControllerBase
+    {
+        private readonly RateMyCatContext _context;
+
+        public CatsController(RateMyCatContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cat>>> GetCats()
+        {
+            return await _context.Cats.ToListAsync();
+        }
+
+        // GET: api/Cats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cat>> GetCat(long id)
+        {
+            var cat = await _context.Cats.FindAsync(id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return cat;
+        }
+
+        // PUT: api/Cats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCat(long id, Cat cat)
+        {
+            if (id != cat.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cat>> PostCat(Cat cat)
+        {
+            _context.Cats.Add(cat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCats), new { id = cat.Id }, cat);
+        }
+
+        // DELETE: api/Cats/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCat(long id)
+        {
+            var cat = await _context.Cats.FindAsync(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cats.Remove(cat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CatExists(long id)
+        {
+            return _context.Cats.Any(e => e.Id == id);
+        }
+    }
+}
